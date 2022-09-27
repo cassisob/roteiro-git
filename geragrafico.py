55cd13b31d096e093572b3931904adc311699d59
Cassiano Sobierai <77468989+cassisob@users.noreply.github.com>
2022-09-26 23:29:07 -0300

    Rotulando linhas e inserindo legenda no gráfico

--- geragrafico.py
+++ geragrafico.py
@@ -15,7 +15,11 @@ def main():
 
     plt.title('Gráfico de linhas')
 
+    i = 1
     for serie in valores:
-       plt.plot(serie)
+        plt.plot(serie, label='Série ' + str(i))   
+        i += 1
+    plt.legend(loc='upper left')
+
  
 main()
